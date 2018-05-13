aa2a4b5959274cda4842b2bfc541396f055e42a4
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-13 14:32:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-""
+"select category, sum(amount) from projects inner join pledges on project_id = projects.id where category='books';"
 end
