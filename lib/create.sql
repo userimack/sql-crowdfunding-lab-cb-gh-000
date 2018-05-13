41b624689a2aa57c6778e294d66b60ce0c5cac9e
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-13 13:54:23 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,18 @@
 create table project(
 title text,
 category integer,
-funding_goal
+funding_goal integer,
+start_date date,
+end_date date,
+);
+
+create table user (
+name text,
+age integer,
+);
+
+create table pledge(
+amount integer,
+user_id 
+project_id 
 )
